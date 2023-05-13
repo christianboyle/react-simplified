3852644d4577e77fef74c2283f54e706d260afd3
Christian Boyle <mail@christianboyle.com>
2023-05-13 10:24:40 -0500

    project: user list, kyles approach

--- 37-user-list-project/src/App.jsx
+++ 37-user-list-project/src/App.jsx
@@ -1,12 +1,47 @@
-import UserComponent from './UserComponent'
+import { useEffect, useState } from 'react'
+import { User } from './User'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const controller = new AbortController()
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      signal: controller.signal
+    })
+      .then((res) => res.json())
+      .then(setUsers)
+      .catch((error) => {
+        if (error.name === 'AbortError') {
+          console.log('Fetch aborted')
+        } else {
+          console.error('Another error happened:', error)
+        }
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
+
   return (
     <>
       <h1>User List</h1>
-      <ul>
-        <UserComponent />
-      </ul>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <ul>
+          {users.map((user) => {
+            return <User key={user.id} name={user.name} />
+          })}
+        </ul>
+      )}
     </>
   )
 }
